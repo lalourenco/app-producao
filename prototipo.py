d616f238ddd0a0cac6204250c8c227d4399a5afa
Luís Antonio Lourenço <99027570+lalourenco@users.noreply.github.com>
2026-06-11 16:36:05 -0300

    Mostrar resultados da avaliação

--- prototipo.py
+++ prototipo.py
@@ -566,17 +566,25 @@ if st.session_state["smd_executado"]:
         # Resultados
 
         impacto = ols_model.params['gt']
+        p_valor = ols_model.pvalues['gt']
 
         st.subheader("📈 Resultado da Análise de Impacto")
 
-        st.metric(
-            "Impacto estimado da intervenção",
-            f"{impacto:.2f}"
-        )
+       if p_valor < 0.05:
+            st.success(
+                f"✅ Houve evidência estatística de impacto da intervenção "
+                f"(coeficiente = {impacto:.2f}; p = {p_valor:.4f})."
+            )
 
-        st.write(
-            f"O efeito estimado da intervenção foi de **{impacto:.2f} anos de vida perdidos por morte por mil habitantes**."
-        )
+            st.write(
+                f"O efeito estimado da intervenção foi de **{impacto:.2f} anos de vida perdidos por morte por mil habitantes no grupo intervenção em comparação ao grupo controle**."
+            )
+         
+        else:
+            st.warning(
+                f"⚠️ Não há evidência estatística suficiente para afirmar que a intervenção gerou resultado"
+            )
+
 
         if st.button("Mostrar análise estatística completa"):
             st.text(st.session_state["ols_model"].summary().as_text())
